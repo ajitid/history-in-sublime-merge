c964212ce4c5fa4744fd0622e53e008c08e09fc0
Ajit <zlksnkwork@gmail.com>
2024-02-03 17:46:44 +0530

    feat: add prettier config and format

--- src/extension.ts
+++ src/extension.ts
@@ -1,56 +1,50 @@
-import * as vscode from 'vscode';
-import * as child from 'child_process';
-import * as findUp from 'find-up';
-import * as path from 'path';
+import * as vscode from "vscode";
+import * as child from "child_process";
+import * as findUp from "find-up";
+import * as path from "path";
 
 type FileDetails = {
-  path: string
-  currentLineNumber: number
-  repository: string
+  path: string;
+  currentLineNumber: number;
+  repository: string;
 };
 
 let SMERGE_BINARY_PATH: string;
 
 const getCurrentRepository = async (file: string): Promise<string> => {
-  const repository = await findUp('.git', {
+  const repository = await findUp(".git", {
     cwd: path.dirname(file),
-    type: 'directory',
+    type: "directory",
   });
 
-  return path.dirname(repository ?? '');
+  return path.dirname(repository ?? "");
 };
 
 const openSublimeMerge = (args: string[], repository: string): void => {
   const customPath = vscode.workspace
     .getConfiguration()
-    .get<string>('history-in-sublime-merge.path');
+    .get<string>("history-in-sublime-merge.path");
 
   child.execFile(customPath || SMERGE_BINARY_PATH, args, {
     cwd: repository,
   });
 };
 
-const getFileDetails = async (
-  editor: vscode.TextEditor
-): Promise<FileDetails> => {
-  const repository: string = await getCurrentRepository(
-    editor.document.uri.path
-  );
+const getFileDetails = async (editor: vscode.TextEditor): Promise<FileDetails> => {
+  const repository: string = await getCurrentRepository(editor.document.uri.path);
 
   return {
-    path: editor.document.uri.path.replace(`${repository}/`, ''),
+    path: editor.document.uri.path.replace(`${repository}/`, ""),
     currentLineNumber: editor.selection.active.line + 1,
-    repository: repository ?? '',
+    repository: repository ?? "",
   };
 };
 
 const viewFileHistory = async (): Promise<void> => {
   if (vscode.window.activeTextEditor) {
-    const { path, repository } = await getFileDetails(
-      vscode.window.activeTextEditor
-    );
+    const { path, repository } = await getFileDetails(vscode.window.activeTextEditor);
 
-    openSublimeMerge(['search', `file:"${path}"`], repository);
+    openSublimeMerge(["search", `file:"${path}"`], repository);
   }
 };
 
@@ -61,10 +55,7 @@ const viewLineHistory = async (): Promise<void> => {
     );
 
     openSublimeMerge(
-      [
-        'search',
-        `file:"${path}" line:${currentLineNumber}-${currentLineNumber}`,
-      ],
+      ["search", `file:"${path}" line:${currentLineNumber}-${currentLineNumber}`],
       repository
     );
   }
@@ -72,40 +63,32 @@ const viewLineHistory = async (): Promise<void> => {
 
 const blameFile = async (): Promise<void> => {
   if (vscode.window.activeTextEditor) {
-    const { path, repository } = await getFileDetails(
-      vscode.window.activeTextEditor
-    );
+    const { path, repository } = await getFileDetails(vscode.window.activeTextEditor);
 
-    openSublimeMerge(['blame', path], repository);
+    openSublimeMerge(["blame", path], repository);
   }
 };
 
 const getSmergeBinaryPath = () => {
   switch (process.platform) {
-    case 'win32':
-      return 'smerge';
-    case 'darwin':
-      return '/Applications/Sublime\ Merge.app/Contents/SharedSupport/bin/smerge';
+    case "win32":
+      return "smerge";
+    case "darwin":
+      return "/Applications/Sublime Merge.app/Contents/SharedSupport/bin/smerge";
     default:
-      return '/opt/sublime_merge/sublime_merge';
+      return "/opt/sublime_merge/sublime_merge";
   }
 };
 
 export const activate = (context: vscode.ExtensionContext): void => {
-  const extensionName = 'history-in-sublime-merge';
+  const extensionName = "history-in-sublime-merge";
   SMERGE_BINARY_PATH = getSmergeBinaryPath();
 
   context.subscriptions.push(
-    vscode.commands.registerCommand(
-      `${extensionName}.viewFileHistory`,
-      viewFileHistory
-    )
+    vscode.commands.registerCommand(`${extensionName}.viewFileHistory`, viewFileHistory)
   );
   context.subscriptions.push(
-    vscode.commands.registerCommand(
-      `${extensionName}.viewLineHistory`,
-      viewLineHistory
-    )
+    vscode.commands.registerCommand(`${extensionName}.viewLineHistory`, viewLineHistory)
   );
   context.subscriptions.push(
     vscode.commands.registerCommand(`${extensionName}.blameFile`, blameFile)
